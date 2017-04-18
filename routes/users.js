15d147508a922b66cf12188eff1f6ec62b81abaa
Yuki Ishikawa <mazeltov17@gmail.com>
2017-04-18 23:09:13 +0900

    wipp

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const mysql = require('mysql');
+const bodyParser = require('body-parser');
 
 
 /* GET users listing. */
@@ -28,6 +29,31 @@ router.get('/', function(req, res, next) {
 	connection.end();
 });
 
+router.post('/create', function(req, res, next) {
+	const connection = mysql.createConnection({
+		host: 'localhost',
+		user: 'hoge',
+		password: 'hogehoge',
+		database: 'node_test'
+	});
+	connection.connect(function(err) {
+		if (err) {
+			console.error(`error connecting: ${err.stack}`);
+			return;
+		}
+		console.log(`connected as id : ${connection.threadId}`);
+	});
+
+	console.log(`res: ${res.body}`);
+
+	connection.query('insert into user ?', {name: res.name, birth: '2017-4-4'}, function(err, results, filelds) {
+		console.log(`error: ${err}`);
+		console.log(results);
+		res.redirect(302, '/users');
+	});
+
+	connection.end();
+});
 
 
 
