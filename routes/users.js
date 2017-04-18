743f777c4c9b93b25542207367fb307b0d250df6
Yuki Ishikawa <mazeltov17@gmail.com>
2017-04-18 23:13:48 +0900

    wiip

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,9 @@ var router = express.Router();
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
 
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
